4c48cec9829b6c9f4cdca1c4bd182eb0944b9594
redthecoderist1000 <redochavillo@gmail.com>
2024-08-30 00:37:56 +0800

    added node-modules

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
 
