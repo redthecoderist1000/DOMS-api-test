aeebc2a23a6875ce73f20d7f9b2abc2bbfa1570a
redthecoderist1000 <redochavillo@gmail.com>
2024-09-07 01:00:06 +0800

    added login command

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
-})
+const dotenv = require("dotenv").config();
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+const { stringify } = require("querystring");
+
+
+app.use(bodyParser.json());
 
 app.get('/hello2', (req, res) => {
   res.send('Hello World runaway!')
@@ -13,3 +17,33 @@ app.get('/hello2', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+var con = mysql.createConnection({
+  host:process.env.DB_HOST,
+  user:process.env.DB_USER,
+  password:process.env.DB_PASS,
+  database:process.env.MYSQL_DB
+});
+
+
+// commands
+
+
+app.post('/login', (req, res) => {
+
+  var post_data = req.body;
+  var studId = post_data.studentID;
+  var pass = post_data.password;
+   
+  con.query("SELECT tbl_stud.student_id, tbl_stud.f_name, tbl_stud.m_name, tbl_stud.l_name, tbl_sect.name as section, tbl_course.name as course ,tbl_course.description as courseName, tbl_stud.email, tbl_stud.gender, tbl_stud.account_status FROM sql12729827.tbl_student_info as tbl_stud INNER JOIN sql12729827.tbl_section_info as tbl_sect ON tbl_stud.section_id = tbl_sect.section_id INNER JOIN sql12729827.tbl_course_info as tbl_course ON tbl_stud.course_id = tbl_course.course_id WHERE tbl_stud.student_id = ? && tbl_stud.password = ?",[studId, pass],(err,result,fields)=>{
+
+    if(err){
+      return console.log(err);
+    }
+    return console.log(result);
+  })
+
+  
+})
+
